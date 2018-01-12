c4a679495d2a75507a2140a7d069767ecbbded34
frenkzappa <camba_franco@yahoo.com>
2018-01-12 14:58:27 +0100

    Cleaned the app.js file from unused lines

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-
-//Module declaration
-//var Slack = require('slack-node');
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var webhookUtil = require('./bots-js-utils/webhook/webhookUtil.js');
 var quagga = require(__dirname + '/quagga');
 
-
 var FBlib = require('./FBLib.js');
 
-// Global variable declaration
 var app = express();
 var port = process.env.PORT;
 var botURL = "http://140.86.14.175:8080/botsui/(botId:C4C4224A-1D0A-4CC7-8A54-623BDC48588B)/bot/intents";
 var botID = "C4C4224A-1D0A-4CC7-8A54-623BDC48588B";
-//USEFUL LINK -----> https://developers.facebook.com/docs/messenger-platform/getting-started/webhook-setup
 
 var facebook_webhook = "https://mediaset-imagebarcode.herokuapp.com/webhook";
 var webhook_verify_token = "123456789";
@@ -29,16 +23,16 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var metadata = {
 	waitForMoreResponsesMs: 200,
-	FacebookBotID: "494482767617258", // from slack app credentials
-	Page_ID: "1067280970047460",
+	FacebookBotID: "494482767617258", // NEEDED BY WEBHOOK SERVER
 	channelSecretKey: '4VPjkK5A9Rvc69U40Lndxq4mctBukFB6', // from bot ui channel webhook
-	channelUrl: 'https://c8c68d92.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/838C7A55-3395-4120-AA79-DF2B55633904'
+	channelUrl: 'https://c8c68d92.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/838C7A55-3395-4120-AA79-DF2B55633904' // FACEBOOK CONNECTOR ADDRESS FOR THE SPECIFIC BOT
 };
 
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
+
 
 // ======== WEBHOOK VERIFICATION =============
 app.get('/webhook', (req, res) => {
@@ -71,28 +65,19 @@ app.get('/webhook', (req, res) => {
 
 
 	// ========= WEBHOOK POST CHANNEL ==========
-	app.post('/webhook', (req, res) => {  
-		 //console.log("I am getting something !!!");
+	app.post('/webhook', (req, res) => {  		 
 		 let body = req.body;
 		 var ispostBack = 0;
-		 // SENDING TO BOT
-		 //console.log("FROM USER TO BOT: " + req.body.entry.messaging.message);
+		 // SENDING TO BOT		 
 		 console.log("PRINTING REQ: " + JSON.stringify(req.body));		 
 		 if(req.body.entry[0].messaging[0].postback){
+			// --- HANDLING GET STARTED BUTTON 
 			if(req.body.entry[0].messaging[0].postback.payload == "Ciao"){
 				console.log("POSTBACK INIZALE DOPO IL CLICK SU INIZIA");
 				//console.log("printing user profile: " + JSON.stringify(FBlib.GetUserProfile("1503067119808430",PAGE_ACCESS_TOKEN)));				
 				ispostBack = 1;
 				console.log("ISpostBack", ispostBack);
-				FBlib.FBsendTextMessage("1503067119808430", "Salve, come ti posso aiutare ?", PAGE_ACCESS_TOKEN);
-				//FBlib.QuickRepliesBuilder("1503067119808430", "Sai con quale decoder guardi Mediaset Premium?", ["Si","No"], PAGE_ACCESS_TOKEN);
-				//webhookUtil.messageToBotWithProperties(metadata.channelUrl, metadata.channelSecretKey, metadata.FacebookBotID, "NextState", additionalProperties, function(err) {			  
-				//	if (err) {
-				//			console.log('Failed to send message to bot.\n');
-				//		}
-				//	});
-				
-			   
+				FBlib.FBsendTextMessage("1503067119808430", "Salve, come ti posso aiutare ?", PAGE_ACCESS_TOKEN);				   
 			} else {
 				console.log("INVIANDO POSTBACK AL BOT");
 				var textToSend = req.body.entry[0].messaging[0].postback.payload;
@@ -108,14 +93,7 @@ app.get('/webhook', (req, res) => {
 		    }
 			if((req.body.entry[0].messaging[0].sender) && (req.body.entry[0].messaging[0].message) && (ispostBack === 0) && !(req.body.entry[0].messaging[0].message.hasOwnProperty("attachments")) )
 						{
-							console.log("RICEVENDO MESSAGGIO DI TESTO O IMMAGINE");
-							//additionalProperties = {
-							//	"userProfile": {
-							//	  "firstName": "Franco",
-							//	  "lastName": "Camba",
-							//	  "age": "29"
-							//	}
-							//  };
+							console.log("RICEVENDO MESSAGGIO DI TESTO O IMMAGINE");							
 							console.log(JSON.stringify(req.body));
 							console.log("FROM CHAT: " + JSON.stringify(req.body.entry[0].messaging[0].message.text));
 							var additionalProperties = { "userProfile": { "clientType": "Facebook" } };
@@ -130,20 +108,11 @@ app.get('/webhook', (req, res) => {
 							if(ispostBack === 0){
 								if (req.body.entry[0].messaging[0].message.hasOwnProperty("attachments")){
 									console.log("RECEIVING ONE IMAGE");
-									quagga.BarCodeRecognition(req.body.entry[0].messaging[0].message.attachments[0].payload.url);
-									//console.log("code", code);
-									//webhookUtil.messageToBotWithProperties(metadata.channelUrl, metadata.channelSecretKey, metadata.FacebookBotID, code, additionalProperties, function(err) {			  
-									//	if (err) {
-									//			console.log('Failed to send message to bot.\n');
-									//		}
-									//	});								
+									quagga.BarCodeRecognition(req.body.entry[0].messaging[0].message.attachments[0].payload.url);																
 								} else {
 										console.log("FROM ORABOT: " + (req.body.entry[0].messaging[0].message));
 										var outputText = (req.body.entry[0].messaging[0].message);
-										FBlib.FBsendTextMessage("1503067119808430", outputText, PAGE_ACCESS_TOKEN);
-										//FBlib.QuickRepliesBuilder("1503067119808430", "provando quick replies", ["uno","due", "tre"], PAGE_ACCESS_TOKEN);
-										//sendTextMessage("1503067119808430",(req.body.entry[0].messaging[0].message));
-										
+										FBlib.FBsendTextMessage("1503067119808430", outputText, PAGE_ACCESS_TOKEN);									
 										}
 								}
 							}
@@ -151,15 +120,9 @@ app.get('/webhook', (req, res) => {
 		 var command = req.body.text;
 		 var additionalProperties = { "userProfile": { "clientType": "Facebook" } };
 		 
-		
-
-		 // Checks this is an event from a page subscription
-		 if (body.object === 'page') {
-	   
-		   // Iterates over each entry - there may be multiple if batched
-		   body.entry.forEach(function(entry) {
-	   
-			 // Gets the message. entry.messaging is an array, but will only ever contain one message, so we get index 0
+		 if (body.object === 'page') {	   
+		   
+		   body.entry.forEach(function(entry) {			 
 			 let webhookEvent = entry.messaging[0];
 			 console.log(webhookEvent);
 		   });
@@ -173,10 +136,8 @@ app.get('/webhook', (req, res) => {
 		 }		
 	});
 
-	// =========== FROM BOT TO FACEBOOK =============
-	 	app.post('/botReply', (req, res)=>{	
-		//console.log("FROM BOT TO USER: " + JSON.stringify(req.body.text));		
-		//if (req.body.text) {
+	// =========== PROCESSING BOT RESPONSE ---- DIFFERENT HANDLING BASED ON TEXT, LIST OR PAYLOAD
+	 	app.post('/botReply', (req, res)=>{			
 		if (req.body.text && !(req.body.choices)) {
 			console.log("Hey you entered a simple TEXT response");
 			var botReply = {
@@ -206,6 +167,7 @@ app.get('/webhook', (req, res) => {
 				}
 			);		
 		} 
+		// ---- RECEIVING A LIST FROM BOT ENGINE ----
 		if (req.body.choices)
 		{
 			console.log("Hey you entered a LIST type response");
